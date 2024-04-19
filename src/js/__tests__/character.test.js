0529bbd77ba6eeba662be22d99f27dcf29164894
Evgeniy Chausov <evgeniy@chausov.ru>
2024-04-19 14:37:43 +0300

    C/A HW

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -1,7 +1,7 @@
-import { Character} from '../character';
+import { Character} from '../class/Character';
 
 test.each([
-  [{name: 'м', type: 'Bowman'}, Error],
+  [{name: 'м', type: 'Bowerman'}, Error],
   [{name: 'лучникккккк', type: 'Swordsman'}, Error],
 ])(
 ('should check name %s on the length'),
@@ -19,36 +19,6 @@ test('should check the name', () => {
   expect(result.name).toBe('Лучник');
 });
 
-test.each([
-  [{name: 'маг', type: 'Bowman'}, 25],
-  [{name: 'лучник', type: 'Swordsman'}, 40],
-  [{name: 'мечник', type: 'Magician'}, 10],
-  [{name: 'маг', type: 'Undead'}, 25],
-  [{name: 'лучник', type: 'Zombie'}, 40],
-  [{name: 'мечник', type: 'Daemon'}, 10],
-])(
-('Person %s should check attack by %t type'),
-(person, expected) => {
-  const result = new Character(person.name, person.type);
-
-  expect(result.attack).toBe(expected);
-});
-
-test.each([
-  [{name: 'маг', type: 'Bowman'}, 25],
-  [{name: 'лучник', type: 'Swordsman'}, 10],
-  [{name: 'мечник', type: 'Magician'}, 40],
-  [{name: 'маг', type: 'Undead'}, 25],
-  [{name: 'лучник', type: 'Zombie'}, 10],
-  [{name: 'мечник', type: 'Daemon'}, 40],
-])(
-('Person %s should check defence by %t type'),
-(person, expected) => {
-  const result = new Character(person.name, person.type);
-
-  expect(result.defence).toBe(expected);
-});
-
 test('should check incorrect type', () => {
   function newPerson() {
     new Character('Лучник', 'Swordsmans');
@@ -58,7 +28,7 @@ test('should check incorrect type', () => {
 });
 
 test('should check levelUp', () => {
-  const result = new Character('Лучник', 'Bowman');
+  const result = new Character('Лучник', 'Bowerman');
   result.levelUp();
 
   expect(result.level).toBe(2);
@@ -66,26 +36,11 @@ test('should check levelUp', () => {
 
 test('should check Error in levelUp when health <= 0', () => {
   function newPerson() {
-    const result = new Character('Лучник', 'Bowman');
+    const result = new Character('Лучник', 'Bowerman');
     result.health = 0;
 
     return result.levelUp();
   }
 
   expect(newPerson).toThrow(Error);
-});
-
-test('should check damage', () => {
-  const result = new Character('Лучник', 'Bowman');
-  result.damage(50);
-
-  expect(result.health).toBe(62.5);
-});
-
-test('should check damage when health < 0', () => {
-  const result = new Character('Лучник', 'Bowman');
-  result.health = -1;
-  result.damage(50);
-
-  expect(result.health).toBe(-1);
 });
